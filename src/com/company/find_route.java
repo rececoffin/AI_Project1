bbb21eee9eb66ce987cbd5367811d690ed867bf8
Rece Coffin <rcoffin@mymail.mines.edu>
2018-02-06 19:41:25 -0700

    Added BFS and output for path details

--- src/com/company/find_route.java
+++ src/com/company/find_route.java
@@ -1,16 +1,17 @@
 package com.company;
 
+import org.omg.CORBA.NO_IMPLEMENT;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class find_route {
     //node and edge classes to store information
     private class Node {
         public String name;
+        public Node parent;
         public Node(String name){
             this.name = name;
         }
@@ -28,9 +29,9 @@ public class find_route {
         }
     }
     private class Edge{
-        public String dest;
+        public Node dest;
         public int cost;
-        public Edge(String dest, int cost){
+        public Edge(Node dest, int cost){
             this.dest = dest;
             this.cost = cost;
         }
@@ -48,8 +49,110 @@ public class find_route {
         this.originCity = new Node(args[1]);
         this.destinationCity = new Node(args[2]);
         initMap();
+        //find a path using breadth first search
+        search();
     }
 
+    private void search() {
+        //queue for BFS
+        LinkedList<Node> queue = new LinkedList<>();
+        Map<Node, Boolean> visited = initVisited();
+        Map<Node, Node> parents = new HashMap<>();
+        visited.put(originCity, true);
+        //begin the queue
+        queue.add(originCity);
+
+        //search
+        Node city;
+        while(!queue.isEmpty()){
+            city = queue.poll();
+//            System.out.println(city.name);
+            //get all adj
+            for(Edge e : the_map.get(city)){
+                Node n = e.dest;
+                //check if visited and add
+                if(!visited.get(n)){
+                    visited.put(n, true);
+                    n.parent = city;
+                    parents.put(n, city);
+                    queue.add(n);
+                }
+            }
+        }
+        //get the solution
+        getSolution(parents);
+    }
+
+    //helper function to init a map
+    private Map<Node, Boolean> initVisited() {
+        Map<Node, Boolean> q = new HashMap<>();
+        for(Node n : the_map.keySet()){
+            q.put(n, false);
+        }
+        return q;
+    }
+
+    //get the desired solution
+    private void getSolution(Map<Node, Node> parents) {
+        ArrayList<Node> solution = new ArrayList<>();
+        ArrayList<Integer> distances = new ArrayList<>();
+        Node n = destinationCity;
+        while(!n.equals(originCity)){
+            solution.add(n);
+            distances.add(getDistance(n, parents.get(n)));
+            n = parents.get(n);
+            //if null, there is not path
+            if(n == null){
+                noPath();
+                System.exit(0);
+            }
+        }
+        //print the solution
+        solution.add(n);
+        printSolution(solution, parents, distances);
+    }
+
+    //function is called if not path is found
+    private void noPath() {
+        System.out.println("distance: infinity");
+        System.out.println("route:");
+        System.out.println("none");
+    }
+
+    private void printSolution(ArrayList<Node> solution, Map<Node, Node> parents,
+                                                       ArrayList<Integer> distances) {
+        //reverse the solution
+        int size = solution.size();
+        Collections.reverse(solution);
+        Collections.reverse(distances);
+        //print the total distance first
+        System.out.println("distance: " + distances.stream().mapToInt(Integer::intValue).sum() + " km");
+        //print the complete route
+        System.out.println("route:");
+        for(int i = 0; i < size - 1; i+=2){
+            Node start = solution.get(i);
+            Node dest = solution.get(i + 1);
+            System.out.println(start.name + " to " + dest.name + ", "
+                                + distances.get(i) + " km");
+        }
+        //print the last leg of the trip
+        Node start = solution.get(size - 2);
+        Node dest = solution.get(size - 1);
+        System.out.println(start.name + " to " + dest.name + ", "
+                            + distances.get(distances.size() - 1) + " km");
+
+    }
+
+    //get the distance from the map
+    private int getDistance(Node start, Node dest) {
+        for(Edge e : the_map.get(start)){
+            if(e.dest.equals(dest)) return e.cost;
+        }
+        return 0;
+    }
+
+
+    //initilize the adj lists
     private void initMap() {
         //read the file, stop at terminating line
         try {
@@ -64,7 +167,7 @@ public class find_route {
                 //parse and build map
                 String[] lineArr = line.split(" ");
                 Node n = new Node(lineArr[0]);
-                Edge e = new Edge( lineArr[1], Integer.parseInt(lineArr[2]));
+                Edge e = new Edge(new Node(lineArr[1]), Integer.parseInt(lineArr[2]));
                 //add to the map
                 if(the_map.containsKey(n)){
                     the_map.get(n).add(e);
@@ -76,7 +179,7 @@ public class find_route {
                 }
                 //add the reverse edge too
                 n = new Node(lineArr[1]);
-                e = new Edge(lineArr[0], Integer.parseInt(lineArr[2]));
+                e = new Edge(new Node(lineArr[0]), Integer.parseInt(lineArr[2]));
                 if(the_map.containsKey(n)){
                     the_map.get(n).add(e);
                 }
@@ -90,18 +193,7 @@ public class find_route {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        printMap();
-    }
-
-    //print the adj list to verify
-    private void printMap() {
-        for(Node n : the_map.keySet()){
-            System.out.print(n.name + ": ");
-            for(Edge e : the_map.get(n)){
-                System.out.print(e.dest + "->" + e.cost + " ");
-            }
-            System.out.println("\n");
-        }
+//        printMap();
     }
 
     //run the program
